8307bdddec92f19cf5e6c90da4858415b1f67cf8
Diego Veramendi <v.dvm.dvm@gmail.com>
2023-09-21 23:06:56 -0500

    Merged 'Forgot password' module

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import Link from 'next/link';
+import Link from 'next/link'
+import '@/styles/Button.css';
 
-const Button = ({ type, text, href }) => (
-  <Link href={href}>
-    <button
-      type={type}
-      style={{ 
-        backgroundColor: 'white', 
-        fontSize: '1.2rem', 
-        color: 'black' 
-      }}
-    >
-      {text}
-    </button>
-  </Link>
-);
+function Button({ href, text }) {
+    return (
+        <Link href={href}>
+            <button className="button" type="button">
+              {text}
+            </button>
+        </Link>
+    );
+}
 
 export default Button;
