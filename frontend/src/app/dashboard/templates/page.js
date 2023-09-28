0b37cb89f11a5b6dd42355bf32be58aeb60e3f9b
Diego Veramendi <v.dvm.dvm@gmail.com>
2023-09-28 16:46:06 -0500

    Reformatted template page for components

--- frontend/src/app/dashboard/templates/page.js
+++ frontend/src/app/dashboard/templates/page.js
@@ -1,6 +1,24 @@
 import React from "react";
 import Card from "@/components/Card";
+import TextField from "@/components/TextField";
 
 export default function Templates() {
-    return <div></div>;
+    return (
+        <>
+            <div className="flex flex-row space-x-4 mb-4">
+                <h2 className="montserrat text-[#172B4D] font-bold text-2xl">
+                    Mis plantillas
+                </h2>
+                <img src="/icons/info-circle.svg" alt="Informacion"></img>
+            </div>
+            <div className="flex flex-row space-x-4 mb-4 w-full">
+                <TextField
+                    placeholder="Buscar plantilla"
+                    width={400}
+                    iconAfter={<img src="/icons/icon-searchBar.svg" />}
+                />
+            </div>
+            <div></div>
+        </>
+    );
 }
