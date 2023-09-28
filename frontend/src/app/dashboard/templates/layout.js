eee3d0e373a0d281d390f66b5259fc391a6a93fa
Diego Veramendi <v.dvm.dvm@gmail.com>
2023-09-28 11:08:18 -0500

    Added tailwind reference and template directory

---
+++ frontend/src/app/dashboard/templates/layout.js
@@ -0,0 +1,7 @@
+export default function RootLayout({ children }) {
+    return (
+        <div className="p-5">
+            {children}
+        </div>
+    );
+}
---
+++ frontend/src/app/dashboard/templates/page.js
@@ -0,0 +1,13 @@
+import LandingFooter from "@/components/LandingFooter";
+import LandingMain from "@/components/LandingMain";
+import LandingNav from "@/components/LandingNav";
+import styles from "@/styles/landing.module.css";
+
+
+export default function Templates() {
+    return (
+        <div>
+            Templates
+        </div>
+    );
+}
