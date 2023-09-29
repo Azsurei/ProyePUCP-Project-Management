2665eed063ab2595ed48d2a94e183e4db8713500
Juan Angelo <111475642+JuanAngelo1@users.noreply.github.com>
2023-09-29 16:59:31 -0500

    xd

--- frontend/src/components/TimePicker1.jsx
+++ frontend/src/components/TimePicker1.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import { Label } from '@atlaskit/form';
 
 import { DatePicker } from '@atlaskit/datetime-picker';
+import { parseISO } from 'date-fns';
 
 
 const getCurrentDateFormatted = () => {
   const now = new Date();
-  const year = now.getFullYear();
-  const month = `${now.getMonth() + 1}`.padStart(2, '0');
   const day = `${now.getDate()}`.padStart(2, '0');
+  const month = `${now.getMonth() + 1}`.padStart(2, '0');
+  const year = now.getFullYear();
   return `${year}-${month}-${day}`;
 };
 
@@ -17,9 +18,11 @@ const TimePicker = () => (
   <>
     
     <DatePicker
+      locale={'es-PE'}
       dateFormat="DD-MM-YYYY"
       defaultValue={getCurrentDateFormatted()}
       placeholder={getCurrentDateFormatted()}
+      
       selectProps={{
         inputId: 'default-date-picker-example',
       }}
