f71ec5377cd3afa98b923b413bbca772e0ba3c92
AVTong <avtong@pucp.edu.pe>
2023-09-29 15:32:56 -0500

    Test

--- frontend/src/app/dashboard/[project]/Equipo/page.js
+++ frontend/src/app/dashboard/[project]/Equipo/page.js
@@ -1,21 +1,30 @@
 "use client"
+import TextInfoCard from "@/components/dashboardComps/projectComps/appConstComps/TextInfoCard";
 import CardEquipo from "@/components/equipoComps/CardEquipo";
 import "@/styles/dashboardStyles/projectStyles/EquipoStyles/Equipo.css";
+import ProgressBar from "@/components/equipoComps/ProgressBar";
+
 const grupos = [
     {
         id: 1,
         nombre: "Módulo 1 - Front End",
         coordinador: "Diego Iwasaki",
+        bgcolor: "#ef6c00", 
+        completed: 53, 
     },
     {
         id: 2,
         nombre: "Módulo 2 - Front End",
         coordinador: "Diego Iwasaki",
+        bgcolor: "#ef6c00", 
+        completed: 53, 
     },
     {
         id: 3,
         nombre: "Módulo 3 - Front End",
         coordinador: "Diego Iwasaki",
+        bgcolor: "#ef6c00", 
+        completed: 53, 
     },
 ];
 
@@ -25,12 +34,20 @@ export default function Equipo() {
             <div className="header">
                 Inicio / Proyectos / Nombre del proyecto / Equipos
             </div>
-            <div className="flex flex-row items-start justify-between w-full">
+            <div className="title">Equipos</div>
+            <div className="butonAddProject">
+                    <a href="/dashboard/[project]/Equipo/nuevo_equipo">
+                        <button className="addProjectbtn">Crear Equipo</button>
+                    </a>
+                </div>
+            <div className="flex flex-row items-start justify-between w-500">
                 {grupos.map((grupo) => (
                     <CardEquipo
                         key={grupo.id}
                         nombre={grupo.nombre}
                         coordinador={grupo.coordinador}
+                        bgcolor={grupo.bgcolor}
+                        completed={grupo.completed}
                     />
                 ))}
             </div>
