90803a0aaece40505157b07c86cb9bd17c78702c
darkoso7 <96436492+darkoso7@users.noreply.github.com>
2023-09-29 15:14:23 -0500

    Everything responsive in ActaConstitucion.jsx

--- frontend/src/components/dashboardComps/projectComps/appConstComps/TextInfoCard.jsx
+++ frontend/src/components/dashboardComps/projectComps/appConstComps/TextInfoCard.jsx
@@ -3,14 +3,22 @@ import CardItem from "./CardItem";
 import "../../../../styles/dashboardStyles/projectStyles/actaConstStyles/TextInfoCard.css";
 
 const TextInfoCard = ({ title, data }) => {
+    const isSingleItem = data.length === 1;
+
     return (
         <div className="project-card">
             <h2 className="project-card__title">{title}</h2>
             {data.map((item, index) => (
-                <CardItem key={index} label={item.label} value={item.value} />
+                <CardItem
+                    key={index}
+                    label={item.label}
+                    value={item.value}
+                    fullWidth={isSingleItem}  // Pass the condition as a prop
+                />
             ))}
         </div>
     );
 };
 
 export default TextInfoCard;
+
