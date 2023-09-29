dcf84b97032e38e786fb2baf9d763d39447f2b78
Diego Veramendi <v.dvm.dvm@gmail.com>
2023-09-29 10:16:23 -0500

    Fixed layout multiple basic rendering

--- frontend/src/app/login/layout.js
+++ frontend/src/app/login/layout.js
@@ -1,10 +1,7 @@
-import styles from "@/styles/landing.module.css";
-import "@/styles/globals.css";
-
 export default function RootLayout({ children }) {
     return (
-        <html lang="es">
-            <body className={`${styles.body} global-styles`}>{children}</body>
-        </html>
+        <>
+            {children}
+        </>
     );
 }
