373d6b098da57895f071b8378e90a4352d303f81
Azsurei <a20203298@pucp.edu.pe>
2023-09-29 10:36:07 -0500

    pequeñas modificaciones al registerPB

--- frontend/src/components/dashboardComps/projectComps/productBacklog/iconLabel.jsx
+++ frontend/src/components/dashboardComps/projectComps/productBacklog/iconLabel.jsx
@@ -3,7 +3,7 @@ import "@/styles/dashboardStyles/projectStyles/productBacklog/IconLabel.css";
 export default function IconLabel({icon,label,className}) {
     return (
         <div className={className}>
-            <img src={icon}/>
+            <img src={icon} className="icono"/>
             <div className="label">
                 {label}
             </div>
