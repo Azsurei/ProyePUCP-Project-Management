a6c17d0e1b4ca17d79bd480c1dead8d0d3600dcd
Diego Veramendi <v.dvm.dvm@gmail.com>
2023-09-30 12:01:28 -0500

    Refactor login layout

--- frontend/src/app/login/layout.js
+++ frontend/src/app/login/layout.js
@@ -1,7 +1,19 @@
+import "@/styles/recoverPassword.css";
+import "@/styles/resetPassword.css";
+
 export default function RootLayout({ children }) {
     return (
-        <>
-            {children}
-        </>
+        <div className="App">
+            <div>
+                <img src="/images/LogoPUCPwhite.png" className="logoPucp"></img>
+                <img
+                    src="/images/LogoProyePUCPwhite.png"
+                    className="logoProyePucp"
+                ></img>
+            </div>
+            <div className="contenedor-principal">
+                {children}
+            </div>
+        </div>
     );
 }
