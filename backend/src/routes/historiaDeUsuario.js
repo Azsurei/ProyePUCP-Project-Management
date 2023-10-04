3f7a9558e00a27ba8c1a2d85c778cf9219201870
gabo52 <gabrielduranruiz@gmail.com>
2023-10-03 21:45:50 -0500

    middlewares e insertar proyecto

--- backend/src/routes/historiaDeUsuario.js
+++ backend/src/routes/historiaDeUsuario.js
@@ -1,79 +1,57 @@
 const express = require('express');
 const connection = require('../config/db');
-const jwt = require("jsonwebtoken");
+const {verifyToken} = require('../middleware/middlewares');
 
-const secret = "oaiscmawiocnaoiwncioawniodnawoinda";
 const routerHistoriaDeUsuario = express.Router();
 
-routerHistoriaDeUsuario.post("/eliminarHistoria",async(req,res)=>{
+routerHistoriaDeUsuario.post("/eliminarHistoria",verifyToken,async(req,res)=>{
 
-    const { tokenProyePUCP } = req.cookies;
-
-    try{
-        const payload = jwt.verify(tokenProyePUCP, secret);
-        console.log(payload);
-        const idHU = req.body.idHistoriaDeUsuario;
+    const idHU = req.body.idHistoriaDeUsuario;
         //Insertar query aca
-        const query = `
-            CALL ELIMINAR_HISTORIA_DE_USUARIO(?);
-        `;
-        try {
-            const [results] = await connection.query(query,[idHU]);
-            res.status(200).json({
-                HU: results[0],
-                message: "Historia de usuario eliminada correctamente"
-            });
-            console.log(`Se ha eliminado la historia de usuario ${idHU}!`);
-            console.log(results);
-        } catch (error) {
-            console.error("Error al eliminar historia de usuario:", error);
-            res.status(500).send("Error al eliminar historia de usuario: " + error.message);
-        }
-    }catch(error){
-        return res.status(401).send(error.message + " invalid tokenProyePUCP token");
+    const query = `
+        CALL ELIMINAR_HISTORIA_DE_USUARIO(?);
+    `;
+    try {
+        const [results] = await connection.query(query,[idHU]);
+        res.status(200).json({
+            HU: results[0],
+            message: "Historia de usuario eliminada correctamente"
+        });
+        console.log(`Se ha eliminado la historia de usuario ${idHU}!`);
+        console.log(results);
+    } catch (error) {
+        console.error("Error al eliminar historia de usuario:", error);
+        res.status(500).send("Error al eliminar historia de usuario: " + error.message);
     }
 })
 
-routerHistoriaDeUsuario.get("/:idHistoriaDeUsuario/detallesHistoria", async (req, res) => {
+routerHistoriaDeUsuario.get("/:idHistoriaDeUsuario/detallesHistoria",verifyToken, async (req, res) => {
 
-    const { tokenProyePUCP } = req.cookies;
-
-    try{
-        const payload = jwt.verify(tokenProyePUCP, secret);
-        //console.log(payload);
-        const idUsuario = payload.user.id;
-        //Insertar query aca
-
-        const { idHistoriaDeUsuario} = req.params;
-        console.log(`Llegue a recibir solicitud listar HU${idHistoriaDeUsuario}`);
-        const query = `
-            CALL LISTAR_HISTORIA_DE_USUARIO_DETALLES(?);
-        `;
-        try {
-            const [results] = await connection.query(query,[idHistoriaDeUsuario]);
-            res.status(200).json({
-                historiaDeUsuario: results[0],
-                message: "Historia obtenida exitosamente"
-            });
-            console.log(`Se han listado la historia de usuario ${idHistoriaDeUsuario}!`);
-            console.log(results);
-        } catch (error) {
-            console.error("Error al obtener la historia de usuario:", error);
-            res.status(500).send("Error al obtener la historia de usuario: " + error.message);
-        }
-
-    }catch(error){
-        return res.status(401).send(error.message + " invalid tokenProyePUCP token");
+    const { idHistoriaDeUsuario} = req.params;
+    console.log(`Llegue a recibir solicitud listar HU${idHistoriaDeUsuario}`);
+    const query = `
+        CALL LISTAR_HISTORIA_DE_USUARIO_DETALLES(?);
+    `;
+    try {
+        const [results] = await connection.query(query,[idHistoriaDeUsuario]);
+        res.status(200).json({
+            historiaDeUsuario: results[0],
+            message: "Historia obtenida exitosamente"
+        });
+        console.log(`Se han listado la historia de usuario ${idHistoriaDeUsuario}!`);
+        console.log(results);
+    } catch (error) {
+        console.error("Error al obtener la historia de usuario:", error);
+        res.status(500).send("Error al obtener la historia de usuario: " + error.message);
     }
 
-    
 });
 
 routerHistoriaDeUsuario.get("/test/:testId", (req, res) => {
     res.send(req.params);
 });
 
-routerHistoriaDeUsuario.get("/listarHistoriasEstado",async(req,res)=>{
+routerHistoriaDeUsuario.get("/listarHistoriasEstado",verifyToken,async(req,res)=>{
     console.log("Llegue a recibir solicitud listar Historias Estado");
     const query = `
         CALL LISTAR_HISTORIAS_ESTADO;
@@ -91,7 +69,7 @@ routerHistoriaDeUsuario.get("/listarHistoriasEstado",async(req,res)=>{
     }
 })
 
-routerHistoriaDeUsuario.get("/listarHistoriasPrioridad",async(req,res)=>{
+routerHistoriaDeUsuario.get("/listarHistoriasPrioridad",verifyToken,async(req,res)=>{
     console.log("Llegue a recibir solicitud listar Historias Prioridad");
     const query = `
         CALL LISTAR_HISTORIAS_PRIORIDAD;
