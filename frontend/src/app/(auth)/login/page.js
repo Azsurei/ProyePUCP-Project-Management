3cba57229abcb470807bc2788677b57d9925c555
Diego Veramendi <v.dvm.dvm@gmail.com>
2023-10-11 10:00:44 -0500

    Added Login Verifications

--- frontend/src/app/(auth)/login/page.js
+++ frontend/src/app/(auth)/login/page.js
@@ -24,42 +24,73 @@ import { EyeSlashFilledIcon } from "@/../public/icons/EyeSlashFilledIcon";
 axios.defaults.withCredentials = true;
 
 function Login() {
+    // Variables de importaciones
     const router = useRouter();
-    const { data: session, status } = useSession();
+    const { data: session } = useSession();
 
+    // Variables de formulario
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [isVisible, setIsVisible] = useState(false);
+    const [statusForm, setStatusForm] = useState("init");
+    const [loadingCred, setLoadingCred] = useState(false);
+    const [loadingGoogle, setLoadingGoogle] = useState(false);
     const [loginError, setLoginError] = useState(null);
 
+    // Variables adicionales
+    const [isVisible, setIsVisible] = useState(false);
+
+    // Funciones adicionales
     const toggleVisibility = () => setIsVisible(!isVisible);
     const validateEmail = (email) =>
         email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+    const validatePassword = (password) => password.length >= 6;
 
-    const isInvalid = React.useMemo(() => {
+    // Control de flujo de variables de formulario
+    const emailInvalid = React.useMemo(() => {
         if (email === "") return false;
         return validateEmail(email) ? false : true;
     }, [email]);
 
+    const passwordInvalid = React.useMemo(() => {
+        if (password === "") return false;
+        return validatePassword(password) ? false : true;
+    }, [password]);
+
+    // Control de flujo del estado general del formulario
     useEffect(() => {
-        if (session && status === "authenticated") {
-            console.log("Inicio de sesión exitoso.");
-            handleSuccessfulLogin();
+        if (statusForm === "init" || statusForm === "valid") {
+            setLoadingCred(false);
+        } else if (statusForm === "submitting") {
+            setLoadingCred(true);
+            setLoginError(null);
         }
-    }, [session, status]);
+    }, [statusForm]);
 
-    const handleSuccessfulLogin = () => {
-        Cookies.set("tokenProyePUCP", session.user.token, { expires: null });
-        router.push("/dashboard");
-    };
+    useEffect(() => {
+        if (
+            emailInvalid ||
+            passwordInvalid ||
+            email === "" ||
+            password === ""
+        ) {
+            setStatusForm("init");
+        } else {
+            setStatusForm("valid");
+        }
+    }, [email, password]);
 
-    const handleGoogleSignIn = async () => {
-        await signIn("google");
-    };
+    // Manejo de envíos de formulario de inicio de sesion
+    useEffect(() => {
+        if (session) {
+            Cookies.set("tokenProyePUCP", session.user.token, {
+                expires: null,
+            });
+            router.push("/dashboard");
+        }
+    }, [session]);
 
     const handleSubmit = async () => {
-        setLoading(true);
+        setStatusForm("submitting");
 
         const responseNextAuth = await signIn("credentials", {
             email,
@@ -68,13 +99,18 @@ function Login() {
         });
 
         if (responseNextAuth.error) {
-            console.log(responseNextAuth.error);
-            setLoading(false);
-            setLoginError(responseNextAuth.error);
+            setStatusForm("valid");
+            setLoginError("Ocurrion un error al iniciar sesión. Intente de nuevo.");
             return;
         }
     };
 
+    const handleGoogleSignIn = async () => {
+        setLoadingGoogle(true);
+        await signIn("google");
+    };
+
+    // Componente general
     return (
         <>
             <p className="flex flex-col items-center font-['Montserrat'] font-medium text-4xl">
@@ -97,10 +133,10 @@ function Login() {
                     variant="bordered"
                     radius="sm"
                     fullWidth={true}
-                    isInvalid={isInvalid}
-                    color={isInvalid ? "danger" : "default"}
+                    isInvalid={emailInvalid}
+                    color={emailInvalid ? "danger" : "default"}
                     errorMessage={
-                        isInvalid ? "Correo electrónico inválido" : ""
+                        emailInvalid ? "Correo electrónico inválido" : ""
                     }
                     onValueChange={setEmail}
                     isClearable
@@ -114,6 +150,13 @@ function Login() {
                     variant="bordered"
                     radius="sm"
                     fullWidth={true}
+                    isInvalid={passwordInvalid}
+                    color={passwordInvalid ? "danger" : "default"}
+                    errorMessage={
+                        passwordInvalid
+                            ? "La contraseña debe tener 6 caracteres o más"
+                            : ""
+                    }
                     onValueChange={setPassword}
                     endContent={
                         <button
@@ -137,7 +180,12 @@ function Login() {
                 text="Iniciar Sesión"
                 href={"#"}
                 onClick={handleSubmit}
-                isLoading={loading}
+                isLoading={loadingCred}
+                isDisabled={
+                    loadingCred ||
+                    statusForm === "init" ||
+                    statusForm === "submitting"
+                }
                 className={"w-48"}
             />
 
@@ -149,8 +197,9 @@ function Login() {
 
             <Button
                 text="Google"
-                ic
                 iconBefore={<img src="/icons/icon-google.svg" />}
+                isLoading={loadingGoogle}
+                isDisabled={loadingGoogle}
                 className={"w-48"}
                 onClick={() => handleGoogleSignIn()}
             />
--- frontend/src/components/dashboardComps/DashboardNav.jsx
+++ frontend/src/components/dashboardComps/DashboardNav.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import Link from "next/link";
@@ -8,6 +8,11 @@ import { signOut } from "next-auth/react";
 import Cookies from "js-cookie";
 
 function DashboardNav({ userName, userLastName }) {
+    const handleSignOut = async () => {
+        Cookies.remove("tokenProyePUCP");
+        await signOut();
+    };
+
     return (
         <nav className="DashboardNav">
             <img
@@ -38,9 +43,20 @@ function DashboardNav({ userName, userLastName }) {
                     <img src="/icons/icon-notif.svg" alt="" className="icon" />
                     <p className="textGuide">Notificaciones</p>
                 </li>
+                <li onClick={handleSignOut}>
+                    <img
+                        src="/icons/icon-signout.svg"
+                        alt=""
+                        className="icon"
+                    />
+                    <p className="textGuide">Cerrar sesión</p>
+                </li>
                 <li>
                     <Link href="/dashboard/templates">
-                        <p className="profilePic">{userName[0]}{userLastName[0]}</p>
+                        <p className="profilePic">
+                            {userName[0]}
+                            {userLastName[0]}
+                        </p>
                     </Link>
                     <p className="textGuide">Mi perfil</p>
                 </li>
