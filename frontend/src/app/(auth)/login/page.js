17b7f523d2cde6635092de9b03f8d1978a28dda0
Diego Veramendi <v.dvm.dvm@gmail.com>
2023-10-17 11:04:21 -0500

    Added email verification

--- frontend/src/app/(auth)/login/page.js
+++ frontend/src/app/(auth)/login/page.js
@@ -41,8 +41,7 @@ function Login() {
 
     // Funciones adicionales
     const toggleVisibility = () => setIsVisible(!isVisible);
-    const validateEmail = (email) => true;
-        // email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+    const validateEmail = (email) => email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
     const validatePassword = (password) => password.length >= 5;
 
     // Control de flujo de variables de formulario
