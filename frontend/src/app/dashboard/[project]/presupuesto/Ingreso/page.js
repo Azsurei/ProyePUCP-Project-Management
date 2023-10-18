621021bc8c31580196d5534d3b0adc47dc615a03
Juan Angelo <111475642+JuanAngelo1@users.noreply.github.com>
2023-10-18 01:39:35 -0500

    Presupuesto Navegacion + headers

---
+++ frontend/src/app/dashboard/[project]/presupuesto/Ingreso/page.js
@@ -0,0 +1,82 @@
+"use client"
+import { useState, useEffect, useCallback, use } from "react";
+import Link from "next/link";
+import React from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
+
+import "@/styles/dashboardStyles/projectStyles/presupuesto/presupuesto.css";
+
+axios.defaults.withCredentials = true;
+import {
+    Input,
+    Button,
+    DropdownTrigger,
+    Dropdown,
+    DropdownMenu,
+    DropdownItem,
+    Pagination,
+  } from "@nextui-org/react";
+import { SearchIcon } from "@/../public/icons/SearchIcon";
+import { PlusIcon } from "@/../public/icons/PlusIcon";
+import { SmallLoadingScreen } from "../../layout";
+
+
+export default function Ingresos(props) {
+    const { setIsLoadingSmall } = useContext(SmallLoadingScreen);
+
+    const decodedUrl = decodeURIComponent(props.params.project);
+    const projectId = decodedUrl.substring(decodedUrl.lastIndexOf("=") + 1);
+    const projectName = decodedUrl.substring(0, decodedUrl.lastIndexOf("="));
+    
+    console.log(projectId);
+    console.log(projectName);
+    //const router=userRouter();
+
+
+
+    const [filterValue, setFilterValue] = React.useState("");
+
+    useEffect(()=>{setIsLoadingSmall(false)},[])
+    return (
+        //Presupuesto/Ingreso
+        <div className="mainDivPresupuesto">
+                <div className="header">
+                     Inicio/Proyectos/{projectName}/Presupuesto/Ingresos
+                </div>
+
+                <div className="presupuesto">
+                    <div className="titlePresupuesto">Ingresos</div>
+
+                    <div className="buttonsPresu">
+                        <Link href={"/dashboard/"+projectName+"="+projectId+"/presupuesto"}>
+                                <button className="btnCommon btnFlujo  sm:w-1 sm:h-1" type="button">Flujo</button>
+                        </Link>
+
+                        <Link href={"/dashboard/"+projectName+"="+projectId+"/presupuesto/Historial"}>
+                                <button className="btnCommon btnHistorial sm:w-1 sm:h-1" type="button">Historial</button> 
+                        </Link>
+                        
+                        <Link href={"/dashboard/"+projectName+"="+projectId+"/presupuesto/Ingreso"}>
+                                <button className="btnCommon btnIngreso btnDisabled btnSelected sm:w-1 sm:h-1"  disabled type="button">Ingresos</button>
+                        </Link>
+
+                        <Link href={"/dashboard/"+projectName+"="+projectId+"/presupuesto/Egresos"}>
+                                <button className="btnCommon btnEgreso sm:w-1 sm:h-1" type="button">Egresos</button>
+                        </Link>
+
+                        <Link href={"/dashboard/"+projectName+"="+projectId+"/presupuesto/Estimacion"}>
+                                <button className="btnCommon btnEstimacion sm:w-1 sm:h-1" type="button">Estimacion</button>
+                        </Link>
+                    </div>
+
+
+                
+                </div>
+        </div>
+    );
+}
+
+
+
