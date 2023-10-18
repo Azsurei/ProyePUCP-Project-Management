678ad1101fd2720641049fb87dda21280bfc4d6b
Azsurei <a20203298@pucp.edu.pe>
2023-10-18 11:46:39 -0500

    arreglando bug del icono de cada usuario en backlog

--- frontend/src/app/dashboard/[project]/matrizDeComunicaciones/registerMC/page.js
+++ frontend/src/app/dashboard/[project]/matrizDeComunicaciones/registerMC/page.js
@@ -3,10 +3,14 @@ import "@/styles/dashboardStyles/projectStyles/MComunicationStyles/registerMC.cs
 import axios from "axios";
 import { useEffect, useState, useContext } from "react";
 import { SmallLoadingScreen } from "../../layout";
+import { Textarea } from "@nextui-org/react";
 axios.defaults.withCredentials = true;
 
-export default function MatrizComunicacionesRegister(props){
+export default function MatrizComunicacionesRegister(props) {
     const { setIsLoadingSmall } = useContext(SmallLoadingScreen);
+    const [sumilla, setSumilla] = useState("");
+    const [detalle, setDetalle] = useState("");
+    const [grupoReceptor, setGrupoReceptor] = useState("");
 
     useEffect(() => {
         setIsLoadingSmall(false);
@@ -15,10 +19,50 @@ export default function MatrizComunicacionesRegister(props){
     return (
         <div className="containerRegisterMC">
             <div className="headerRegisterMC">
-                Inicio / Proyectos / Nombre del proyecto / Matriz de Comunicaciones/ Registrar elemento
+                Inicio / Proyectos / Nombre del proyecto / Matriz de
+                Comunicaciones/ Registrar elemento
             </div>
-            <div className="titleBacklogRegisterMC">
-                Crear nueva información requerida
+            <div className="backlogRegisterMC">
+                <div className="titleBacklogRegisterMC">
+                    Crear nueva información requerida
+                </div>
+                <div>
+                    <Textarea
+                        label="Sumilla de la información requerida"
+                        labelPlacement="outside"
+                        placeholder="Escriba aquí"
+                        isRequired
+                        className="custom-label"
+                        value={sumilla}
+                        onValueChange={setSumilla}
+                    />
+                </div>
+                <div className="comboMC">
+
+                </div>
+                <div>
+                    <Textarea
+                        label="Detalle de la información requerida"
+                        labelPlacement="outside"
+                        placeholder="Escriba aquí"
+                        isRequired
+                        className="custom-label"
+                        minRows="5"
+                        value={detalle}
+                        onValueChange={setDetalle}
+                    />
+                 </div>   
+                 <div>
+                    <Textarea
+                        label="Grupo receptor"
+                        labelPlacement="outside"
+                        placeholder="Escriba aquí"
+                        isRequired
+                        className="custom-label"
+                        value={grupoReceptor}
+                        onValueChange={setGrupoReceptor}
+                    />
+                 </div>
             </div>
         </div>
     );
