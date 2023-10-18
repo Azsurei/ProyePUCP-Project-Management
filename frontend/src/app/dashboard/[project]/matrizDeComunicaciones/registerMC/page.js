01a1a646bb5ba1790fb0d4a70b35549a793535e2
Azsurei <a20203298@pucp.edu.pe>
2023-10-17 23:28:01 -0500

    empezando registerMC

--- frontend/src/app/dashboard/[project]/matrizDeComunicaciones/registerMC/page.js
+++ frontend/src/app/dashboard/[project]/matrizDeComunicaciones/registerMC/page.js
@@ -1,6 +1,25 @@
 "use client";
+import "@/styles/dashboardStyles/projectStyles/MComunicationStyles/registerMC.css";
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { SmallLoadingScreen } from "../../layout";
 axios.defaults.withCredentials = true;
 
 export default function MatrizComunicacionesRegister(props){
-    
+    const { setIsLoadingSmall } = useContext(SmallLoadingScreen);
+
+    useEffect(() => {
+        setIsLoadingSmall(false);
+    }, []);
+
+    return (
+        <div className="containerRegisterMC">
+            <div className="headerRegisterMC">
+                Inicio / Proyectos / Nombre del proyecto / Matriz de Comunicaciones/ Registrar elemento
+            </div>
+            <div className="titleBacklogRegisterMC">
+                Crear nueva información requerida
+            </div>
+        </div>
+    );
 }
