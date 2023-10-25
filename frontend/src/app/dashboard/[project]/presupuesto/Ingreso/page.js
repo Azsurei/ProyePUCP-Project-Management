64398f5c4cfe7857c6aa4ff685586d98028dad70
Juan Angelo <angelo.flores@pucp.edu.pe>
2023-10-25 11:10:02 -0500

    update 1/3 modal Ingreso

--- frontend/src/app/dashboard/[project]/presupuesto/Ingreso/page.js
+++ frontend/src/app/dashboard/[project]/presupuesto/Ingreso/page.js
@@ -320,6 +320,7 @@ export default function Ingresos(props) {
                                                 idParam="idMoneda"
                                                 initialName="Tipo Moneda"
                                                 inputWidth="2/3"
+                                                widthCombo="9"
                                             />
 
                                             </div>
@@ -377,6 +378,7 @@ export default function Ingresos(props) {
                                                 idParam="idTransaccionTipo"
                                                 initialName="Seleccione Ingreso"
                                                 inputWidth="64"
+                                                widthCombo="15"
                                             />
 
                                         </div>
@@ -394,6 +396,7 @@ export default function Ingresos(props) {
                                                 idParam="idIngresoTipo"
                                                 initialName="Seleccione Transacción"
                                                 inputWidth="64"
+                                                widthCombo="15"
                                             />
 
                                         </div>
