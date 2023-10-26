514950c05063a9eea7a075ecea283bd4dd1c77d5
Diego Veramendi <v.dvm.dvm@gmail.com>
2023-10-26 18:45:31 -0500

    Finished autoevaluacion roles

--- frontend/src/app/dashboard/[project]/autoevaluacionEquipo/page.js
+++ frontend/src/app/dashboard/[project]/autoevaluacionEquipo/page.js
@@ -11,6 +11,7 @@ import {
     CardHeader,
     CardBody,
     Textarea,
+    Input,
     Radio,
     RadioGroup,
     Modal,
@@ -19,6 +20,7 @@ import {
     ModalBody,
     ModalFooter,
     useDisclosure,
+    user,
 } from "@nextui-org/react";
 import { Breadcrumbs, BreadcrumbsItem } from "@/components/Breadcrumb";
 import { Toaster, toast } from "sonner";
@@ -28,6 +30,7 @@ axios.defaults.withCredentials = true;
 export default function autoevaluacionEquipo(props) {
     const { data: session } = useSession();
     const userId = session?.user?.id.toString();
+    const rol = session?.user?.rol;
 
     const decodedUrl = decodeURIComponent(props.params.project);
     const projectId = decodedUrl.substring(decodedUrl.lastIndexOf("=") + 1);
@@ -36,58 +39,112 @@ export default function autoevaluacionEquipo(props) {
 
     const [initialEvaluations, setInitialEvaluations] = useState([]);
     const [usersEvaluation, setUsersEvaluation] = useState(initialEvaluations);
-    const [formState, setFormState] = useState("initial"); // "initial", "modified"
+    const [formState, setFormState] = useState("initial"); // "empty", "created", "initial", "modified"
+    const [isLoading, setIsLoading] = useState(false);
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-    // Manejo de carga y guardado de datos
-    useEffect(() => {
-        axios
-            .get(
+    // Manejo de carga de datos
+    const getEvaluation = async () => {
+        try {
+            const response = await axios.get(
                 `http://localhost:8080/api/proyecto/autoEvaluacion/listarAutoEvaluacion/` +
                     projectId +
                     `/` +
                     userId
-            )
-            .then((response) => {
+            );
+
+            if (response.status === 200) {
+                if (rol !== 1) {
+                    setFormState("created");
+                    return;
+                }
                 const evaluaciones = response.data.evaluados;
                 setInitialEvaluations(evaluaciones);
                 setUsersEvaluation(evaluaciones);
-            })
-            .catch((error) => {
-                console.error("Error al obtener las evaluaciones.", error);
-            });
-        setIsLoadingSmall(false);
+            } else if (response.status === 204) {
+                setFormState("empty");
+            }
+        } catch (error) {
+            console.error("Error al obtener las evaluaciones.", error);
+        } finally {
+            setIsLoadingSmall(false);
+        }
+    };
+
+    useEffect(() => {
+        getEvaluation();
     }, []);
-    
+
+    // Manejo de guardado de datos
     function saveEvaluation() {
         return new Promise((resolve, reject) => {
-            axios.put(
-                `http://localhost:8080/api/proyecto/autoEvaluacion/actualizarAutoEvaluacion`,
-                {
-                    evaluados: usersEvaluation,
-                }
-            )
-            .then((response) => {
-                console.log(response);
-                resolve();
-            })
-            .catch((error) => {
-                console.error("Error al guardar las evaluaciones: ", error);
-                reject(error);
-            });
+            axios
+                .put(
+                    `http://localhost:8080/api/proyecto/autoEvaluacion/actualizarAutoEvaluacion`,
+                    {
+                        evaluados: usersEvaluation,
+                    }
+                )
+                .then((response) => {
+                    console.log(response);
+                    resolve();
+                })
+                .catch((error) => {
+                    console.error("Error al guardar las evaluaciones: ", error);
+                    reject(error);
+                });
         });
     }
 
     const handleSave = async () => {
-        setFormState("loading");
+        setIsLoading(true);
         try {
             await saveEvaluation();
             setInitialEvaluations(usersEvaluation);
             toast.success("La autoevaluación se ha guardado exitosamente.");
         } catch (e) {
             toast.error("Ha ocurrido un error al guardar la autoevaluación.");
+        } finally {
+            onOpenChange(true);
         }
-        finally {
+    };
+
+    // Manejo de creación de datos
+    function createEvaluation() {
+        return new Promise((resolve, reject) => {
+            axios
+                .post(
+                    `http://localhost:8080/api/proyecto/autoEvaluacion/crearAutoEvaluacion`,
+                    {
+                        idProyecto: projectId,
+                        criterio1: "Dominio técnico",
+                        criterio2: "Compromiso con los trabajos",
+                        criterio3: "Comunicación con los compañeros",
+                        criterio4: "Comprensión de proyecto",
+                    }
+                )
+                .then((response) => {
+                    console.log(response);
+                    resolve();
+                })
+                .catch((error) => {
+                    console.error("Error al crear las evaluaciones: ", error);
+                    reject(error);
+                });
+        });
+    }
+
+    const handleCreate = async () => {
+        setIsLoading(true);
+        try {
+            // Espera de 2segs
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+            await createEvaluation();
+            toast.success("La autoevaluación se ha creado exitosamente.");
+            setFormState("created");
+        } catch (e) {
+            toast.error("Ha ocurrido un error al crear la autoevaluación.");
+        } finally {
             onOpenChange(true);
         }
     };
@@ -143,13 +200,12 @@ export default function autoevaluacionEquipo(props) {
             JSON.stringify(initialEvaluations)
         ) {
             setFormState("modified");
-        }
-        else {
+        } else {
             setFormState("initial");
         }
     }, [usersEvaluation, initialEvaluations]);
 
-    // Renderizado
+    // Renderizado de miembro
     const renderMember = (member) => {
         return (
             <Card className="w-full">
@@ -239,10 +295,184 @@ export default function autoevaluacionEquipo(props) {
         );
     };
 
+    // Renderizado de acuerdo a rol
+    const renderRole = () => {
+        // agregar variantes para renderizado dependiendo del rol del usuario (igual a 1 o diferente)
+        return (
+            <>
+                {formState === "empty" && (
+                    <div className="flex flex-col flex-1 items-center justify-center gap-4 w-full">
+                        <img
+                            src="/images/magic_search.svg"
+                            alt="Autoevaluacion inactiva"
+                        />
+                        <p className="font-[Montserrat] text-xl">
+                            No existe una autoevaluación activa
+                        </p>
+                        {rol === 1 && (
+                            <p className="font-[Montserrat] text-xl">
+                                Un supervisor o jefe de proyecto debe activar la
+                                autoevaluación
+                            </p>
+                        )}
+                        {rol !== 1 && (
+                            <>
+                                <Button
+                                    className="font-[Roboto] bg-[#172B4D] text-[#FFFFFF]"
+                                    onPress={onOpen}
+                                >
+                                    Crear autoevaluación
+                                </Button>
+                                <Modal
+                                    isOpen={isOpen}
+                                    onOpenChange={onOpenChange}
+                                    isDismissable={false}
+                                >
+                                    <ModalContent>
+                                        {(onClose) => (
+                                            <>
+                                                <ModalHeader className="flex flex-col gap-1">
+                                                    Registrar autoevaluación
+                                                </ModalHeader>
+                                                <ModalBody>
+                                                    <Input
+                                                        autoFocus
+                                                        label="Criterio 1"
+                                                        placeholder="Ej. Dominio técnico"
+                                                        type="text"
+                                                        variant="bordered"
+                                                    />
+                                                    <Input
+                                                        label="Criterio 2"
+                                                        placeholder="Ej. Compromiso con los trabjos"
+                                                        type="text"
+                                                        variant="bordered"
+                                                    />
+                                                    <Input
+                                                        label="Criterio 3"
+                                                        placeholder="Ej. Comunicación con los compañeros"
+                                                        type="text"
+                                                        variant="bordered"
+                                                    />
+                                                    <Input
+                                                        label="Criterio 4"
+                                                        placeholder="Ej. Comprensión de proyecto"
+                                                        type="text"
+                                                        variant="bordered"
+                                                    />
+                                                </ModalBody>
+                                                <ModalFooter>
+                                                    <Button
+                                                        className="font-[Roboto]"
+                                                        color="default"
+                                                        variant="light"
+                                                        onPress={onClose}
+                                                    >
+                                                        Cerrar
+                                                    </Button>
+                                                    <Button
+                                                        className="font-[Roboto] bg-[#172B4D] text-[#FFFFFF]"
+                                                        onPress={handleCreate}
+                                                        isLoading={
+                                                            isLoading === true
+                                                        }
+                                                    >
+                                                        Crear
+                                                    </Button>
+                                                </ModalFooter>
+                                            </>
+                                        )}
+                                    </ModalContent>
+                                </Modal>
+                            </>
+                        )}
+                    </div>
+                )}
+                {formState === "created" && (
+                    <div className="flex flex-col flex-1 items-center justify-center gap-4 w-full">
+                        <img
+                            src="/images/magic_search.svg"
+                            alt="Autoevaluacion activa"
+                        />
+                        <p className="font-[Montserrat] text-xl">
+                            La autoevaluación se encuentra activa
+                        </p>
+                    </div>
+                )}
+                {rol === 1 && formState !== "empty" && (
+                    <>
+                        <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center items-right sm:gap-10 gap-4 mb-4">
+                            <p className="text-justify">
+                                Califique a cada uno de sus compañeros del 1 al
+                                5, incluyendo su propio trabajo. Siendo el 1 la
+                                calificación más baja y 5 la calificación más
+                                alta
+                            </p>
+                            <Button
+                                className="bg-[#172B4D] text-[#FFFFFF]"
+                                onPress={onOpen}
+                                isDisabled={formState === "initial"}
+                            >
+                                Guardar
+                            </Button>
+                        </div>
+                        <div className="flex flex-col justify-between items-center gap-8 mb-4">
+                            {usersEvaluation.map((user) => (
+                                <div
+                                    key={user.idUsuarioEvaluado}
+                                    className="w-full"
+                                >
+                                    {renderMember(user)}
+                                </div>
+                            ))}
+                        </div>
+                        <Modal
+                            isOpen={isOpen}
+                            onOpenChange={onOpenChange}
+                            isDismissable={false}
+                        >
+                            <ModalContent>
+                                {(onClose) => (
+                                    <>
+                                        <ModalHeader className="flex flex-col gap-1">
+                                            Guardar cambios
+                                        </ModalHeader>
+                                        <ModalBody>
+                                            <p>
+                                                ¿Seguro que deseas guardar los
+                                                cambios?
+                                            </p>
+                                        </ModalBody>
+                                        <ModalFooter>
+                                            <Button
+                                                color="default"
+                                                variant="light"
+                                                onPress={onClose}
+                                            >
+                                                Cerrar
+                                            </Button>
+                                            <Button
+                                                className="font-[Roboto] bg-[#172B4D] text-[#FFFFFF]"
+                                                onPress={handleSave}
+                                                isLoading={isLoading === true}
+                                            >
+                                                Guardar
+                                            </Button>
+                                        </ModalFooter>
+                                    </>
+                                )}
+                            </ModalContent>
+                        </Modal>
+                    </>
+                )}
+            </>
+        );
+    };
+
     // Componente
     return (
         <div className="flex flex-col p-8 w-full h-full">
-            <Toaster richColors closeButton={true}/>
+            <Toaster richColors closeButton={true} />
             <div className="space-x-4 mb-2">
                 <Breadcrumbs>
                     <BreadcrumbsItem
@@ -258,57 +488,7 @@ export default function autoevaluacionEquipo(props) {
             <h2 className="space-x-4 mb-2 montserrat text-[#172B4D] font-bold text-3xl text-gray-700">
                 Autoevaluación del equipo
             </h2>
-            <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center items-right sm:gap-10 gap-4 mb-4">
-                <p className="text-justify">
-                    Califique a cada uno de sus compañeros del 1 al 5,
-                    incluyendo su propio trabajo. Siendo el 1 la calificación
-                    más baja y 5 la calificación más alta
-                </p>
-                <Button
-                    className="bg-[#172B4D] text-[#FFFFFF]"
-                    onPress={onOpen}
-                    isDisabled={formState === "initial"}
-                >
-                    Guardar
-                </Button>
-            </div>
-            <div className="flex flex-col justify-between items-center gap-8 mb-4">
-                {usersEvaluation.map((user) => (
-                    <div key={user.idUsuarioEvaluado} className="w-full">
-                        {renderMember(user)}
-                    </div>
-                ))}
-            </div>
-            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-                <ModalContent>
-                    {(onClose) => (
-                        <>
-                            <ModalHeader className="flex flex-col gap-1">
-                                Guardar cambios
-                            </ModalHeader>
-                            <ModalBody>
-                                <p>¿Seguro que deseas guardar los cambios?</p>
-                            </ModalBody>
-                            <ModalFooter>
-                                <Button
-                                    color="default"
-                                    variant="light"
-                                    onPress={onClose}
-                                >
-                                    Cerrar
-                                </Button>
-                                <Button
-                                    className="bg-[#172B4D] text-[#FFFFFF]"
-                                    onPress={handleSave}
-                                    isLoading={formState === "loading"}
-                                >
-                                    Guardar
-                                </Button>
-                            </ModalFooter>
-                        </>
-                    )}
-                </ModalContent>
-            </Modal>
+            {renderRole()}
         </div>
     );
 }
