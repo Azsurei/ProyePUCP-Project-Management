eb02199760a6d2452ea0265243788e653fd8a477
Azsurei <a20203298@pucp.edu.pe>
2023-10-27 18:23:48 -0500

    cambios de diseño en equipo

--- frontend/src/app/dashboard/[project]/Equipo/nuevo_equipo/page.js
+++ frontend/src/app/dashboard/[project]/Equipo/nuevo_equipo/page.js
@@ -425,6 +425,7 @@ export default function crear_equipo(props) {
                                     oneButton={false}
                                     secondAction={() => {
                                         checkData();
+                                        setAddParticipantesState(true);
                                     }}
                                     textColor="blue"
                                     verifyFunction={() => {
