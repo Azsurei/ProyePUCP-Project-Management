f6136e33c0089266a50e6fdea239a54811e253a9
rgpq25 <renzopq@hotmail.com>
2023-10-28 23:25:00 -0500

    changing routing to /backlog/productBacklog

--- frontend/src/app/dashboard/[project]/settings/page.js
+++ frontend/src/app/dashboard/[project]/settings/page.js
@@ -63,61 +63,63 @@ export default function Settings() {
     const btnStyleActive = "font-medium px-4 py-2 rounded-md bg-[#F4F4F5] cursor-pointer";
 
     return (
-        <div className="flex flex-col w-[100%] h-[100%] p-[2.5rem] space-y-7 font-[Montserrat]">
-            <div className="flex flex-col">
-                <p className="text-4xl font-semibold text-172B4D">
-                    Configura tu proyecto
-                </p>
-                <p className="text-slate-400">
-                    Maneja usuarios, herramientas, fechas y mas
-                </p>
-            </div>
-
-            <Divider className="px-[50px]"></Divider>
-
-            <div className="flex flex-row w-[100%] h-[100%] space-x-8">
-                <div className="flex flex-col h-[100%] w-[20%] space-y-1">
-                    <p
-                        className={
-                            settingsState === "users"
-                                ? btnStyleActive
-                                : btnStyle
-                        }
-                        onClick={() => {
-                            setSettingsState("users");
-                        }}
-                    >
-                        Usuarios
+        <div className="w-[100%] flex justify-center">
+            <div className="flex flex-col w-[100%] max-w-[1200px] h-[100%] p-[2.5rem] space-y-7 font-[Montserrat]">
+                <div className="flex flex-col">
+                    <p className="text-4xl font-semibold text-172B4D">
+                        Configura tu proyecto
                     </p>
-                    <p
-                        className={
-                            settingsState === "tools"
-                                ? btnStyleActive
-                                : btnStyle
-                        }
-                        onClick={() => {
-                            setSettingsState("tools");
-                        }}
-                    >
-                        Herramientas
-                    </p>
-                    <p
-                        className={
-                            settingsState === "dates"
-                                ? btnStyleActive
-                                : btnStyle
-                        }
-                        onClick={() => {
-                            setSettingsState("dates");
-                        }}
-                    >
-                        Fechas
+                    <p className="text-slate-400">
+                        Maneja usuarios, herramientas, fechas y mas
                     </p>
                 </div>
-
-                {settingsState==="users" && <UsersScreen></UsersScreen>}
-                {settingsState==="tools" && <ToolsScreen></ToolsScreen>}
-                {settingsState==="dates" && <DatesScreen></DatesScreen>}
+    
+                <Divider className="px-[50px]"></Divider>
+    
+                <div className="flex flex-row w-[100%] h-[100%] space-x-8">
+                    <div className="flex flex-col h-[100%] w-[20%] space-y-1">
+                        <p
+                            className={
+                                settingsState === "users"
+                                    ? btnStyleActive
+                                    : btnStyle
+                            }
+                            onClick={() => {
+                                setSettingsState("users");
+                            }}
+                        >
+                            Usuarios
+                        </p>
+                        <p
+                            className={
+                                settingsState === "tools"
+                                    ? btnStyleActive
+                                    : btnStyle
+                            }
+                            onClick={() => {
+                                setSettingsState("tools");
+                            }}
+                        >
+                            Herramientas
+                        </p>
+                        <p
+                            className={
+                                settingsState === "dates"
+                                    ? btnStyleActive
+                                    : btnStyle
+                            }
+                            onClick={() => {
+                                setSettingsState("dates");
+                            }}
+                        >
+                            Fechas
+                        </p>
+                    </div>
+    
+                    {settingsState==="users" && <UsersScreen></UsersScreen>}
+                    {settingsState==="tools" && <ToolsScreen></ToolsScreen>}
+                    {settingsState==="dates" && <DatesScreen></DatesScreen>}
+                </div>
             </div>
         </div>
     );
--- frontend/src/components/dashboardComps/projectComps/productBacklog/RouteringBacklog.jsx
+++ frontend/src/components/dashboardComps/projectComps/productBacklog/RouteringBacklog.jsx
@@ -5,6 +5,6 @@ function RouteringBacklog({proy_name, proy_id, idHu}) {
     const proy_name1 = 'Mi primer proyecto';
     const proy_id1 = '42';
     const idHu1 = '5';
-    router.push("/dashboard/"+proy_name+"="+proy_id+"/productBacklog/" + idHu);
+    router.push("/dashboard/"+proy_name+"="+proy_id+"/backlog/productBacklog/" + idHu);
 };
 export default RouteringBacklog;
