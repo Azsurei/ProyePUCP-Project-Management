94a936798d629ccbbb542708c69629bc08f48315
AVTong <avtong@pucp.edu.pe>
2023-10-31 18:05:08 -0500

    Fix

--- backend/src/routes/catalogoInteresados.js
+++ backend/src/routes/catalogoInteresados.js
@@ -3,7 +3,7 @@ const routerCatalogoInteresados = express.Router();
 const { verifyToken } = require("../middleware/middlewares");
 const catalogoInteresadosController = require("../controllers/catalogoInteresados/catalogoInteresadosController");
 
-routerCatalagoRiesgos.get("/listarAutoridad", catalogoInteresadosController.listarAutoridad);
-routerCatalagoRiesgos.get("/listarAdhesion", catalogoInteresadosController.listarAdhesion);
+routerCatalogoInteresados.get("/listarAutoridad", catalogoInteresadosController.listarAutoridad);
+routerCatalogoInteresados.get("/listarAdhesion", catalogoInteresadosController.listarAdhesion);
 
 module.exports.routerCatalogoInteresados = routerCatalogoInteresados;
