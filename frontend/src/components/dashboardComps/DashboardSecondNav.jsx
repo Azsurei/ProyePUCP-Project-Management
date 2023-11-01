e834f17d6686bd79cddfc3ac2323f46df9fcd12d
Azsurei <a20203298@pucp.edu.pe>
2023-10-31 21:26:53 -0500

    modo oscuro en dashboard

--- frontend/src/components/dashboardComps/DashboardSecondNav.jsx
+++ frontend/src/components/dashboardComps/DashboardSecondNav.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function DashboardSecondNav() {
     return (
-        <ul className="DashboardSecondNav bg-mainHeaders">
+        <ul className="DashboardSecondNav bg-text-mainHeaders">
             <li>
                 <img src="/icons/sidebarIcon1.svg" className='sideIcon'></img>
                 <Link href='/dashboard'><p>Principal</p></Link>
