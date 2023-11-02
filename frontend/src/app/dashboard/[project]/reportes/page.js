17583f699cca1eb7bb1bb53673072fc65825601f
Kitaro1 <iwasaki.diego@gmail.com>
2023-11-01 23:58:41 -0500

    filtros reportes

--- frontend/src/app/dashboard/[project]/reportes/page.js
+++ frontend/src/app/dashboard/[project]/reportes/page.js
@@ -3,15 +3,52 @@ import React from "react";
 import { SmallLoadingScreen } from "../layout";
 import { Breadcrumbs, BreadcrumbsItem } from "@/components/Breadcrumb";
 import { PlusIcon } from "@/../public/icons/PlusIcon";
-import { useContext } from "react";
-import { Button } from "@nextui-org/react";
+import { useContext, useState } from "react";
+import { 
+    Button,
+    Input,
+    DropdownTrigger,
+    Dropdown,
+    DropdownMenu,
+    DropdownItem,
+} from "@nextui-org/react";
 import "@/styles/dashboardStyles/projectStyles/reportesStyles/reportes.css"
+import { SearchIcon } from "@/../public/icons/SearchIcon";
+import TuneIcon from '@mui/icons-material/Tune';
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@nextui-org/react";
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import { ChevronDownIcon } from "@/../public/icons/ChevronDownIcon";
+import AssessmentIcon from '@mui/icons-material/Assessment';
 export default function Reportes(props) {
     const { setIsLoadingSmall } = useContext(SmallLoadingScreen);
     const decodedUrl = decodeURIComponent(props.params.project);
     const projectId = decodedUrl.substring(decodedUrl.lastIndexOf("=") + 1);
     const projectName = decodedUrl.substring(0, decodedUrl.lastIndexOf("="));
+    const [filterValue, setFilterValue] = React.useState("");
+    const [toolsFilter, setToolsFilter] = React.useState("all");
+    const onSearchChange = (value) => {
+        setFilterValue(value);
+    };
+    const [fechaInicio, setFechaInicio] = useState("");
+    const [fechaFin, setFechaFin] = useState("");
+    const [filtrarFecha, setFiltrarFecha] = useState(false);
 
+    const handleChangeFechaInicioFilter = (event) => {
+        setFechaInicio(event.target.value);
+    };
+
+    const handleChangeFechaFinFilter = (event) => {
+        setFechaFin(event.target.value);
+    };
+    const {
+        isOpen: isModalFechaOpen,
+        onOpen: onModalFecha,
+        onOpenChange: onModalFechachange,
+    } = useDisclosure();
+    const toolsOptions = [
+        { name: "Reporte de Avance", uid: "active" },
+        { name: "Grupo de Reporte", uid: "active" },
+    ];
     return (
         <div className="divHistorialReportes">
              <Breadcrumbs>
@@ -28,8 +65,162 @@ export default function Reportes(props) {
                     </Button>
                        
                 </div>
-                
+                <div className="divFiltrosReporte">
+                    <Input
+                        isClearable
+                        className="w-2/4 sm:max-w-[50%]"
+                        placeholder="Buscar reporte..."
+                        startContent={<SearchIcon />}
+                        value={filterValue}
+                        onClear={() => onClear("")}
+                        onValueChange={onSearchChange}
+                        variant="faded"
+                    />
+                    <div className="buttonReporteContainer">
+                    <Button  onPress={onModalFecha} color="primary" startContent={<TuneIcon />} className="btnFiltro">
+                            Filtrar
+                    </Button>
+                    <Dropdown>
+                            <DropdownTrigger className="btnFiltro">
+                                <Button
+                                    endContent={
+                                        <ChevronDownIcon className="text-small" />
+                                    }
+                                    variant="flat"
+                                    className="font-['Roboto']"
+                                >
+                                    Proyectos
+                                </Button>
+                            </DropdownTrigger>
+                            <DropdownMenu
+                                disallowEmptySelection
+                                aria-label="Table Columns"
+                                closeOnSelect={false}
+                                selectedKeys={toolsFilter}
+                                selectionMode="multiple"
+                                onSelectionChange={setToolsFilter}
+                            >
+                                {toolsOptions.map((status) => (
+                                    <DropdownItem
+                                        key={status.uid}
+                                    >
+                                        {status.name}
+                                    </DropdownItem>
+                                ))}
+                            </DropdownMenu>
+                    </Dropdown>
+                    </div>
+                </div>
             </div>
+            <Modal size="xl" isOpen={isModalFechaOpen} onOpenChange={onModalFechachange}>
+                    <ModalContent>
+                        {(onClose) => {
+                        const finalizarModal = () => {
+                            //filtraFecha();
+                            setFiltrarFecha(true);
+                            onClose();
+                        };
+                        return (
+                            <>
+                            <ModalHeader>Filtra tus reportes</ModalHeader>
+
+                            <ModalBody>
+                                <p
+                                style={{
+                                    color: "#494949",
+                                    fontSize: "16px",
+                                    fontStyle: "normal",
+                                    fontWeight: 400,
+                                }}
+                                >
+                                Elige la fecha que deseas consultar
+                                </p>
+
+                                <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "space-between",
+                                    marginTop: "1rem",
+                                }}
+                                >
+                                <p
+                                    style={{
+                                    color: "#002D74",
+                                    fontSize: "16px",
+                                    fontStyle: "normal",
+                                    fontWeight: 500,
+                                    }}
+                                >
+                                    Desde
+                                </p>
+
+                                <p
+                                    style={{
+                                    color: "#002D74",
+                                    fontSize: "16px",
+                                    fontStyle: "normal",
+                                    fontWeight: 500,
+                                    flex: 0.43,
+                                    }}
+                                >
+                                    Hasta
+                                </p>
+                                </div>
+
+                                <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "space-between",
+                                    gap: "2rem",
+                                    marginBottom: "1.2rem",
+                                }}
+                                >
+                                <DateInput
+                                    isEditable={true}
+                                    value={fechaInicio}
+                                    onChangeHandler={handleChangeFechaInicioFilter}
+                                />
+
+                                <span style={{ margin: "0 10px" }}>
+                                    <ArrowRightAltIcon />
+                                </span>
+
+                                <DateInput
+                                    value={fechaFin}
+                                    isEditable={true}
+                                    onChangeHandler={handleChangeFechaFinFilter}
+                                />
+                                </div>
+                            </ModalBody>
+
+                            <ModalFooter>
+                                <Button
+                                className="text-white"
+                                variant="light"
+                                onPress={() => {
+                                    onClose(); // Cierra el modal
+                                    setFechaInicio("");
+                                    setFechaFin("");
+                                }}
+                                style={{ color: "#EA541D" }}
+                                >
+                                Limpiar Búsqueda
+                                </Button>
+                                <Button
+                                style={{ backgroundColor: "#EA541D" }}
+                                className="text-white"
+                                onPress={finalizarModal}
+                                >
+                                Filtrar
+                                </Button>
+                            </ModalFooter>
+                            </>
+                        );
+                        }}
+                    </ModalContent>
+                </Modal>
         </div>
     );
 }
