f9df910369b6ebf1656879fa956dbdfd1ed9ee59
darkoso7 <96436492+darkoso7@users.noreply.github.com>
2023-11-03 12:04:02 -0500

    agrega edit

--- frontend/src/app/dashboard/[project]/actaReunion/page.js
+++ frontend/src/app/dashboard/[project]/actaReunion/page.js
@@ -2,15 +2,16 @@
 "use client";
 
 import "@/styles/dashboardStyles/projectStyles/EDTStyles/EDT.css";
-import React, { useState, useContext, useEffect, createContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { SmallLoadingScreen } from "../layout";
 axios.defaults.withCredentials = true;
-import { Button, Avatar, AvatarGroup, Card , CardBody, CardHeader, Divider} from '@nextui-org/react';
+import {Button, Avatar, AvatarGroup, Card, CardBody, CardHeader, Divider, Spacer} from '@nextui-org/react';
 import {Tabs, Tab} from '@nextui-org/react';
 import HeaderWithButtons from "@/components/dashboardComps/projectComps/EDTComps/HeaderWithButtons";
 import { useRouter } from "next/navigation";
 import Modal from "@/components/dashboardComps/projectComps/productBacklog/Modal";
+import MissingEDTComponents from "../../../../../public/images/missing_EDTComponents.svg";
 
 export default function ActaReunion(props) {
     const { setIsLoadingSmall } = useContext(SmallLoadingScreen);
@@ -25,7 +26,6 @@ export default function ActaReunion(props) {
 // *********************************************************************************************
 // Searching Meeting Record ID
 // *********************************************************************************************
-    const [meetingId, setMeetingId] = useState("");
 
     useEffect(() => {
         setIsLoadingSmall(true);
@@ -38,9 +38,6 @@ export default function ActaReunion(props) {
             })
             .catch((error) => {
                 console.error("Error fetching meeting record ID:", error);
-            })
-            .finally(() => {
-                setIsLoadingSmall(false);
             });
     }, [setIsLoadingSmall, projectId]);
 
@@ -117,77 +114,101 @@ export default function ActaReunion(props) {
             : reunion.participantesXReunion ? [reunion.participantesXReunion] : [];
         return (
 
-            <div key={reunion.idLineaActaReunion} className="flex flex-wrap items-start my-4 space-x-4 justify-center">
-                <Card key={reunion.idLineaActaReunion} className="flex-grow w-full sm:w-72 md:w-80 lg:w-96 xl:w-[400px] mx-auto" isPressable={true}>
-                    <CardHeader className="p-4">
-                        <h3 className="text-xl font-bold text-blue-900 montserrat">{reunion.nombreReunion}</h3>
-                    </CardHeader>
-                    <Divider />
-                    <CardBody className="flex-row justify-between items-center h-36">
-                        <div className="mr-4">
-                            <p className="text-blue-900 montserrat">Reunión convocada por:</p>
-                            <p className="text-blue-900 montserrat">{reunion.nombreConvocante}</p>
-                            <p className="text-gray-700 montserrat">Fecha: {new Date(reunion.fechaReunion).toLocaleDateString()}</p>
-                            <p className="text-gray-700 montserrat">Hora: {reunion.horaReunion.slice(0, 5)}</p>
-                        </div>
-                        {participantes.length > 0 && (
-                            <>
-                                <Divider orientation="vertical" />
-                                <div className="flex text-gray-700 montserrat">Convocados :   </div>
-                                <AvatarGroup isBordered max={3} >
-                                    {participantes.map((participante, index) => (
-                                        <Avatar key={participante.idParticipanteXReunion}  src="" fallback={
-                                            <p className="bg-gray-300 cursor-pointer rounded-full flex justify-center items-center text-base w-12 h-12 text-black">
-                                                {participante.nombres[0] + participante.apellidos[0]}
-                                            </p>
-                                        } />
-                                    ))}
-                                </AvatarGroup>
-                            </>
-                        )}
-                    </CardBody>
-                </Card>
-                <div className="flex flex-col space-y-10 mt-10">
-                    <Button className="w-36 bg-blue-900 text-white font-semibold" onClick={() => handleEdit(reunion)}>Editar</Button>
-                    <Modal
-                        nameButton="Eliminar"
-                        textHeader="Eliminar reunion"
-                        textBody="¿Seguro que quiere eliminar esta reunion?"
-                        colorButton="w-36 bg-red-600 text-white font-semibold"
-                        oneButton={false}
-                        secondAction={() => {
-                            handleDelete(reunion.idLineaActaReunion).then(r => console.log(r));
-                            router.back();
-                        }}
-                        textColor="red"
-                    />
+                <div
+                    key={reunion.idLineaActaReunion}
+                    className="flex flex-wrap items-start my-4 space-x-4 justify-center">
+                    <Card key={reunion.idLineaActaReunion} className="flex-grow w-full sm:w-72 md:w-80 lg:w-96 xl:w-[400px] mx-auto" isPressable={true}>
+                        <CardHeader className="p-4">
+                            <h3 className="text-xl font-bold text-blue-900 montserrat">{reunion.nombreReunion}</h3>
+                        </CardHeader>
+                        <Divider />
+                        <CardBody className="flex-row justify-between items-center h-36">
+                            <div className="mr-4">
+                                <p className="text-blue-900 montserrat">Reunión convocada por:</p>
+                                <p className="text-blue-900 montserrat">{reunion.nombreConvocante}</p>
+                                <p className="text-gray-700 montserrat">Fecha: {new Date(reunion.fechaReunion).toLocaleDateString()}</p>
+                                <p className="text-gray-700 montserrat">Hora: {reunion.horaReunion.slice(0, 5)}</p>
+                            </div>
+                            {participantes.length > 0 && (
+                                <>
+                                    <Divider orientation="vertical" />
+                                    <div className="flex text-gray-700 montserrat">Convocados :   </div>
+                                    <AvatarGroup isBordered max={3} >
+                                        {participantes.map((participante, index) => (
+                                            <Avatar key={participante.idParticipanteXReunion}  src="" fallback={
+                                                <p className="bg-gray-300 cursor-pointer rounded-full flex justify-center items-center text-base w-12 h-12 text-black">
+                                                    {participante.nombres[0] + participante.apellidos[0]}
+                                                </p>
+                                            } />
+                                        ))}
+                                    </AvatarGroup>
+                                </>
+                            )}
+                        </CardBody>
+                    </Card>
+                    <div className="flex flex-col space-y-10 mt-10">
+                        <Button className="w-36 bg-blue-900 text-white font-semibold" onClick={() => handleEdit(reunion)}>Editar</Button>
+                        <Modal
+                            nameButton="Eliminar"
+                            textHeader="Eliminar reunion"
+                            textBody="¿Seguro que quiere eliminar esta reunion?"
+                            colorButton="w-36 bg-red-600 text-white font-semibold"
+                            oneButton={false}
+                            secondAction={() => {
+                                handleDelete(reunion.idLineaActaReunion).then(r => console.log(r));
+                            }}
+                            textColor="red"
+                        />
+                    </div>
                 </div>
-            </div>
-            // Crear botones de editar y eliminar
+
+
         );
     };
 
+    const newHref = '/dashboard/'+projectName+'='+projectId+'/actaReunion/registerAR';
+    const actualHref = '/dashboard/'+ projectName + '=' + projectId + '/actaReunion';
     return (
         <div style={{ padding: '20px', width: '100%', height: '80%' }}>
             <HeaderWithButtons
                 haveReturn={true}
                 haveAddNew={true}
-                hrefToReturn={`/dashboard/${projectName}=${projectId}/actaReunion`}
-                hrefForButton={`/dashboard/${projectName}=${projectId}/actaReunion/registerAR`}
+                hrefToReturn={actualHref}
+                hrefForButton={{
+                    pathname:newHref,
+                    query: {previousUrl: actualHref},
+                }}
                 breadcrump={`Inicio / Proyectos / ${projectName} / Acta de Reunión`}
                 btnText={'+ Agregar reunión'}
             >
                 Acta de Reunión
             </HeaderWithButtons>
 
-            <Tabs aria-label={"Options"} radius={"full"} color={"warning"}>
-                <Tab key={"pending"} title={"Pendientes"} className={"montserrat text-blue-900"}>
-                    {reuniones.pendientes.map(renderCard)}
+            <Tabs aria-label="Options" radius="full" color="warning">
+                <Tab key="pending" title="Pendientes" className="montserrat text-blue-900">
+                    {reuniones.pendientes && reuniones.pendientes.length > 0 ? (
+                        reuniones.pendientes.map(renderCard)
+                    ) : (
+                        <div className="flex flex-col items-center justify-center">
+                            <Spacer  y={55} />
+                            <MissingEDTComponents />
+                            <p className="montserrat text-blue-900">No hay reuniones pendientes</p>
+                        </div>
+                    )}
                 </Tab>
-                <Tab key={"finished"} title={"Finalizados"} className={"montserrat text-blue-900"}>
-                    {reuniones.finalizadas.map(renderCard)}
+                <Tab key="finished" title="Finalizados" className="montserrat text-blue-900">
+                    {reuniones.finalizadas && reuniones.finalizadas.length > 0 ? (
+                        reuniones.finalizadas.map(renderCard)
+                    ) : (
+                        <div className="flex flex-col items-center justify-center">
+                            <Spacer  y={55} />
+                            <MissingEDTComponents />
+                            <p>No hay reuniones finalizadas</p>
+                        </div>
+                    )}
                 </Tab>
             </Tabs>
+
         </div>
     );
 }
