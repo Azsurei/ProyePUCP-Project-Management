d4781a08cd238ce8d02699684bcf698981891402
Azsurei <a20203298@pucp.edu.pe>
2023-11-05 18:17:35 -0500

    modificando la tabla para que funcione con apis y objetos específicos

--- frontend/src/app/dashboard/[project]/matrizDeResponsabilidades/page.js
+++ frontend/src/app/dashboard/[project]/matrizDeResponsabilidades/page.js
@@ -16,80 +16,112 @@ import {
     Tooltip,
     getKeyValue,
 } from "@nextui-org/react";
-const columns = [
-    { name: "Entregables", uid: "entregable" },
-    { name: "Rol 1", uid: "rol1" },
-    { name: "Rol 2", uid: "rol2" },
-    { name: "Rol 3", uid: "rol3" },
-    { name: "Rol 4", uid: "rol4" },
+
+const dataFromApi = [
+    {
+        idRol: 1,
+        nombreRol: "ROL1",
+        idEntregable: 1,
+        nombreEntregable: "Entregable 1",
+        idResponsabilidad: 1,
+        nombreResponsabilidad: "Aprueba",
+    },
+    {
+        idRol: 2,
+        nombreRol: "ROL2",
+        idEntregable: 1,
+        nombreEntregable: "Entregable 1",
+        idResponsabilidad: 1,
+        nombreResponsabilidad: "Aprueba",
+    },
+    {
+        idRol: 3,
+        nombreRol: "ROL3",
+        idEntregable: 1,
+        nombreEntregable: "Entregable 1",
+        idResponsabilidad: 1,
+        nombreResponsabilidad: "Aprueba",
+    },
+    {
+        idRol: 4,
+        nombreRol: "ROL4",
+        idEntregable: 1,
+        nombreEntregable: "Entregable 1",
+        idResponsabilidad: 2,
+        nombreResponsabilidad: "Se le informa",
+    },
+    {
+        idRol: 1,
+        nombreRol: "ROL1",
+        idEntregable: 2,
+        nombreEntregable: "Entregable 2",
+        idResponsabilidad: 2,
+        nombreResponsabilidad: "Se le informa",
+    },
+    {
+        idRol: 2,
+        nombreRol: "ROL2",
+        idEntregable: 2,
+        nombreEntregable: "Entregable 2",
+        idResponsabilidad: 1,
+        nombreResponsabilidad: "Aprueba",
+    },
+    {
+        idRol: 3,
+        nombreRol: "ROL3",
+        idEntregable: 2,
+        nombreEntregable: "Entregable 2",
+        idResponsabilidad: 1,
+        nombreResponsabilidad: "Aprueba",
+    },
+    {
+        idRol: 4,
+        nombreRol: "ROL4",
+        idEntregable: 2,
+        nombreEntregable: "Entregable 2",
+        idResponsabilidad: 1,
+        nombreResponsabilidad: "Aprueba",
+    },
 ];
 
-const rows = [
-    {
-        id: 1,
-        entregable: "Entregable 1",
-        roles: [
-            { rol1: "Rol 1" },
-            { rol2: "Rol 2" },
-            { rol3: "Rol 3" },
-            { rol4: "Rol 4" },
-        ],
-    },
-    {
-        id: 2,
-        entregable: "Entregable 2",
-        roles: [
-            { rol1: "Rol 5" },
-            { rol2: "Rol 6" },
-            { rol3: "Rol 7" },
-            { rol4: "Rol 8" },
-        ],
-    },
-    {
-        id: 3,
-        entregable: "Entregable 3",
-        roles: [
-            { rol1: "Rol 9" },
-            { rol2: "Rol 10" },
-            { rol3: "Rol 11" },
-            { rol4: "Rol 12" },
-        ],
-    },
-    {
-        id: 4,
-        entregable: "Entregable 4",
-        roles: [
-            { rol1: "Rol 13" },
-            { rol2: "Rol 14" },
-            { rol3: "Rol 15" },
-            { rol4: "Rol 16" },
-        ],
-    },
-    {
-        id: 5,
-        entregable: "Entregable 5",
-        roles: [
-            { rol1: "Rol 17" },
-            { rol2: "Rol 18" },
-            { rol3: "Rol 19" },
-            { rol4: "Rol 20" },
-        ],
-    },
+const roles = Array.from(new Set(dataFromApi.map((item) => item.nombreRol)));
+const entregables = Array.from(
+    new Set(dataFromApi.map((item) => item.nombreEntregable))
+);
+
+console.log("Roles", roles);
+console.log("Entregables", entregables);
+
+const columns = [
+    { name: "Entregables", uid: "entregable" },
+    ...roles.map((role) => ({ name: role, uid: role.toLowerCase() })),
 ];
+
+const rows = entregables.map((entregable, index) => {
+    const row = {
+        id: index,
+        entregable,
+    };
+    roles.forEach((role) => {
+        const dataForRoleAndEntregable = dataFromApi.find(
+            (item) =>
+                item.nombreRol === role && item.nombreEntregable === entregable
+        );
+        row[role.toLowerCase()] = dataForRoleAndEntregable
+            ? dataForRoleAndEntregable.nombreResponsabilidad
+            : ""; // Asegúrate de ajustar esto según la propiedad correcta en tus datos
+    });
+    return row;
+});
 
 export default function MatrizDeResponsabilidades(props) {
     const { setIsLoadingSmall } = useContext(SmallLoadingScreen);
     const renderCell = React.useCallback((user, columnKey) => {
-        console.log("EEE",columnKey);
         switch (columnKey) {
             case "entregable":
                 return user[columnKey];
             default:
-                const cellValue = user.roles;
-                const object = cellValue.find(
-                    (obj) => obj[columnKey] !== undefined
-                );
-                return object[columnKey];
+                return user[columnKey];
         }
     }, []);
 
