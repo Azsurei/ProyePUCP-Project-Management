b2ff446351c62cf819759571ec0f191cb0fb5455
rgpq25 <renzopq@hotmail.com>
2023-11-12 17:34:31 -0500

    push

--- frontend/src/app/dashboard/[project]/settings/page.js
+++ frontend/src/app/dashboard/[project]/settings/page.js
@@ -1,14 +1,18 @@
 "use client";
-import { Button, Divider } from "@nextui-org/react";
-import { useEffect, useState } from "react";
+import { Button, Divider, useDisclosure } from "@nextui-org/react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
+import ModalDeleteConfirmation from "@/components/dashboardComps/projectComps/settingsComps/ModalDeleteConfirmation";
+import { SmallLoadingScreen } from "../layout";
 axios.defaults.withCredentials = true;
 
-function UsersScreen () {
+function UsersScreen() {
     return (
         <div className="flex flex-col flex-1 h-[100%] space-y-2">
             <div className="flex flex-col mb-3">
-                <p className="text-2xl font-medium text-mainHeaders">Usuarios</p>
+                <p className="text-2xl font-medium text-mainHeaders">
+                    Usuarios
+                </p>
                 <p className="text-slate-400">
                     Añade usuarios o elimina algunos de tu proyecto
                 </p>
@@ -21,15 +25,14 @@ function UsersScreen () {
     );
 }
 
-
-function ToolsScreen () {
+function ToolsScreen() {
     return (
         <div className="flex flex-col flex-1 h-[100%] space-y-2">
             <div className="flex flex-col mb-3">
-                <p className="text-2xl font-medium text-mainHeaders">Herramientas</p>
-                <p className="text-slate-400">
-                    Personaliza tus herramientas
+                <p className="text-2xl font-medium text-mainHeaders">
+                    Herramientas
                 </p>
+                <p className="text-slate-400">Personaliza tus herramientas</p>
             </div>
 
             <Divider></Divider>
@@ -39,14 +42,12 @@ function ToolsScreen () {
     );
 }
 
-function DatesScreen () {
+function DatesScreen() {
     return (
         <div className="flex flex-col flex-1 h-[100%] space-y-2">
             <div className="flex flex-col mb-3">
                 <p className="text-2xl font-medium text-mainHeaders">Fechas</p>
-                <p className="text-slate-400">
-                    Modifica datos del proyecto
-                </p>
+                <p className="text-slate-400">Modifica datos del proyecto</p>
             </div>
 
             <Divider></Divider>
@@ -61,11 +62,14 @@ function DatesScreen () {
 const deleteProject = () => {
     const idProyecto = 200;
 
-    const resultado = axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/proyecto/${idProyecto}/listarHerramientasDeProyecto`);
+    const resultado = axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/proyecto/${idProyecto}/listarHerramientasDeProyecto`
+    );
     const herramientas = resultado.data.herramientas;
     axios
         .delete(
-            process.env.NEXT_PUBLIC_BACKEND_URL + '/api/proyecto/eliminarProyecto',
+            process.env.NEXT_PUBLIC_BACKEND_URL +
+                "/api/proyecto/eliminarProyecto",
             {
                 idProyecto: idProyecto,
                 herramientas: herramientas,
@@ -80,32 +84,58 @@ const deleteProject = () => {
         });
 };
 
-function DeleteScreen () {
-    return(
+function DeleteScreen({handleDelete}) {
+    return (
         <div className="flex flex-col flex-1 h-[100%] space-y-2">
             <div className="flex flex-col mb-3">
-                <p className="text-2xl font-medium text-mainHeaders">Eliminar Proyecto</p>
+                <p className="text-2xl font-medium text-mainHeaders">
+                    Eliminar Proyecto
+                </p>
                 <p className="text-slate-400">
-                    Elimina tu proyecto, tus herramientas y todos los datos asociados a él. ¡Ten cuidado!
+                    Elimina tu proyecto, tus herramientas y todos los datos
+                    asociados a él. ¡Ten cuidado!
                 </p>
             </div>
 
             <Divider></Divider>
 
-            <Button color="danger" className="max-w-xs w-60">
+            <Button color="danger" className="max-w-xs w-60" onPress={handleDelete}>
                 Eliminar Proyecto
             </Button>
         </div>
     );
 }
 
-export default function Settings() {
+export default function Settings(props) {
+    const decodedUrl = decodeURIComponent(props.params.project);
+    const projectId = decodedUrl.substring(decodedUrl.lastIndexOf("=") + 1);
+    const projectName = decodedUrl.substring(0, decodedUrl.lastIndexOf("="));
+    const { setIsLoadingSmall } = useContext(SmallLoadingScreen);
+
+    const {
+        isOpen: isModalDeleteOpen,
+        onOpen: onModalDeleteOpen,
+        onOpenChange: onModalDeleteChange,
+    } = useDisclosure();
+
     const [settingsState, setSettingsState] = useState("users");
-    const btnStyle = "group hover:underline  font-medium px-4 py-2 rounded-md cursor-pointer";
-    const btnStyleActive = "font-medium px-4 py-2 rounded-md bg-[#F4F4F5] dark:bg-[#414141] cursor-pointer";
+    const btnStyle =
+        "group hover:underline  font-medium px-4 py-2 rounded-md cursor-pointer";
+    const btnStyleActive =
+        "font-medium px-4 py-2 rounded-md bg-[#F4F4F5] dark:bg-[#414141] cursor-pointer";
+
+    useEffect(()=>{
+        setIsLoadingSmall(false);
+    })
 
     return (
         <div className="w-[100%] min-h-full flex justify-center bg-mainBackground">
+            <ModalDeleteConfirmation
+                isOpen={isModalDeleteOpen}
+                onOpenChange={onModalDeleteChange}
+                idProyecto={projectId}
+            />
+
             <div className="flex flex-col w-[100%] max-w-[1200px] h-[100%] p-[2.5rem] space-y-7 font-[Montserrat]">
                 <div className="flex flex-col">
                     <p className="text-4xl font-semibold text-mainHeaders">
@@ -115,9 +145,9 @@ export default function Settings() {
                         Maneja usuarios, herramientas, fechas y mas
                     </p>
                 </div>
-    
+
                 <Divider className="px-[50px]"></Divider>
-    
+
                 <div className="flex flex-row w-[100%] h-[100%] space-x-8">
                     <div className="flex flex-col h-[100%] w-[20%] space-y-1">
                         <p
@@ -169,11 +199,13 @@ export default function Settings() {
                             Eliminar
                         </p>
                     </div>
-    
-                    {settingsState==="users" && <UsersScreen></UsersScreen>}
-                    {settingsState==="tools" && <ToolsScreen></ToolsScreen>}
-                    {settingsState==="dates" && <DatesScreen></DatesScreen>}
-                    {settingsState==="delete" && <DeleteScreen></DeleteScreen>}
+
+                    {settingsState === "users" && <UsersScreen></UsersScreen>}
+                    {settingsState === "tools" && <ToolsScreen></ToolsScreen>}
+                    {settingsState === "dates" && <DatesScreen></DatesScreen>}
+                    {settingsState === "delete" && (
+                        <DeleteScreen handleDelete={onModalDeleteOpen}></DeleteScreen>
+                    )}
                 </div>
             </div>
         </div>
