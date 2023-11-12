491063244662fba71205a9b6472c4edf520f0b2b
rgpq25 <renzopq@hotmail.com>
2023-11-12 16:57:57 -0500

    push

--- backend/src/controllers/grupoProyectos/grupoProyectosController.js
+++ backend/src/controllers/grupoProyectos/grupoProyectosController.js
@@ -10,10 +10,13 @@ async function insertarGrupoProyectos(req, res, next) {
     const query = `CALL INSERTAR_GRUPO_PROYECTO(?,?);`;
     const query1 = `CALL INSERTAR_PROYECTO_EN_GRUPO(?,?);`;
     try {
-        const [results] = await connection.query(query,[nombreGrupo, idUsuario]);
+        const [results] = await connection.query(query, [
+            nombreGrupo,
+            idUsuario,
+        ]);
         const idGrupoDeProyecto = results[0][0].idGrupoDeProyecto;
 
-        for(let proyecto of listProyectos){
+        for (let proyecto of listProyectos) {
             await connection.query(query1, [proyecto, idGrupoDeProyecto]);
         }
         res.status(200).json({
@@ -23,42 +26,41 @@ async function insertarGrupoProyectos(req, res, next) {
         next(error);
     }
 }
-async function listarGruposProyecto(req,res,next){
+async function listarGruposProyecto(req, res, next) {
     const { idUsuario } = req.params;
     try {
         const query = `CALL LISTAR_GRUPO_PROYECTOS_X_ID_USUARIO(?);`;
-        const [results] = await connection.query(query,[idUsuario]);
+        const [results] = await connection.query(query, [idUsuario]);
         const grupos = results[0];
-        res.status(200).json({grupos, message: "Grupos listados"});
+        res.status(200).json({ grupos, message: "Grupos listados" });
     } catch (error) {
         console.log(error);
         next(error);
     }
 }
 
-
-
-
 async function listarProyectosXGrupo(req, res, next) {
-    const { idGrupoProyecto  } = req.params;
+    const { idGrupoProyecto } = req.params;
 
     //de presupuesto
     const query = `CALL LISTAR_PROYECTOS_PRESUPUESTO_X_GRUPO_PROYECTOS(?);`;
     const query1 = `CALL LISTAR_LINEA_INGRESO_X_ID_PRESUPUESTO(?);`;
-    const query2 = `CALL LISTAR_LINEA_EGRESO_X_ID_PRESUPUESTO(?);`;      //trae tareas padres absolutas (o sin hijos)
+    const query2 = `CALL LISTAR_LINEA_EGRESO_X_ID_PRESUPUESTO(?);`; //trae tareas padres absolutas (o sin hijos)
 
     try {
-        const [results] = await connection.query(query,[idGrupoProyecto]);
+        const [results] = await connection.query(query, [idGrupoProyecto]);
         const proyectos = results[0];
-        for(let proyecto of proyectos){
-
-            const [results1] = await connection.query(query1, [proyecto.idPresupuesto]);
+        for (let proyecto of proyectos) {
+            const [results1] = await connection.query(query1, [
+                proyecto.idPresupuesto,
+            ]);
             let ingresos = results1[0];
             proyecto.ingresos = ingresos;
-            const [results2] = await connection.query(query2, [proyecto.idPresupuesto]);
+            const [results2] = await connection.query(query2, [
+                proyecto.idPresupuesto,
+            ]);
             let egresos = results2[0];
             proyecto.egresos = egresos;
-
         }
         res.status(200).json({
             proyectos,
@@ -69,15 +71,13 @@ async function listarProyectosXGrupo(req, res, next) {
     }
 }
 
-
 //un arreglo de proyectos
 //cada proyecto debe tener data de presupuesto
 //cada proyecto debe tener data de entregables
 //cada proyecto debe tener data de tareas
 
-
 async function listarDatosProyectosXGrupo(req, res, next) {
-    const { idGrupoProyecto  } = req.params;
+    const { idGrupoProyecto } = req.params;
 
     //de proyecto
     const query = `CALL LISTAR_PROYECTOS_X_GRUPO_PROYECTOS(?);`;
@@ -88,44 +88,67 @@ async function listarDatosProyectosXGrupo(req, res, next) {
     //de presupuesto
     const queryP1 = `CALL LISTAR_LINEA_INGRESO_X_ID_PRESUPUESTO(?);`;
     const queryP2 = `CALL LISTAR_LINEA_EGRESO_X_ID_PRESUPUESTO(?);`;
-    
+
     //de entregables
-    const queryE1 = `CALL LISTAR_LINEA_INGRESO_X_ID_PRESUPUESTO(?);`
+    const queryE1 = `CALL LISTAR_LINEA_INGRESO_X_ID_PRESUPUESTO(?);`;
 
     try {
-        const [results] = await connection.query(query,[idGrupoProyecto]);
+        const [results] = await connection.query(query, [idGrupoProyecto]);
         const proyectos = results[0];
 
-        for(const proyecto of proyectos){
-            const [resultsB] = await connection.query(queryA1,[proyecto.idProyecto]);
-            console.log(resultsB[0]);
-            const idHerramientas = resultsB[0];
+        for (const proyecto of proyectos) {
+            const [resultsB] = await connection.query(queryA1, [
+                proyecto.idProyecto,
+            ]);
+            const idHerramientas = resultsB[0][0];
+            console.log("TODOS LOS IDS => " + JSON.stringify(idHerramientas));
+
+            proyecto.EDT = {};
+            proyecto.cronograma = {};
+            proyecto.presupuesto = {};
 
             const EDT = {
-                idEDT: idHerramientas.idEDT
-            }
+                idEDT:
+                    idHerramientas.idEDT === undefined ||
+                    idHerramientas.idEDT === null
+                        ? 0
+                        : idHerramientas.idEDT,
+                entregables: [],
+            };
             const cronograma = {
-                idCronograma: idHerramientas.idCronograma
-            }
+                idCronograma:
+                    idHerramientas.idCronograma === undefined ||
+                    idHerramientas.idCronograma === null
+                        ? 0
+                        : idHerramientas.idCronograma,
+                tareas: [],
+            };
             const presupuesto = {
-                idPresupuesto: idHerramientas.idPresupuesto,
+                idPresupuesto:
+                    idHerramientas.idPresupuesto === undefined ||
+                    idHerramientas.idPresupuesto === null
+                        ? 0
+                        : idHerramientas.idPresupuesto,
                 ingresos: [],
                 egresos: [],
-            }
+            };
 
             proyecto.EDT = EDT;
             proyecto.cronograma = cronograma;
             proyecto.presupuesto = presupuesto;
 
             //para presupuesto
-            const [resultsP1] = await connection.query(queryP1, [proyecto.presupuesto.idPresupuesto]);
+            const [resultsP1] = await connection.query(queryP1, [
+                proyecto.presupuesto.idPresupuesto,
+            ]);
             const ingresos = resultsP1[0];
             proyecto.presupuesto.ingresos = ingresos;
 
-            const [resultsP2] = await connection.query(queryP2, [proyecto.presupuesto.idPresupuesto]);
+            const [resultsP2] = await connection.query(queryP2, [
+                proyecto.presupuesto.idPresupuesto,
+            ]);
             const egresos = resultsP2[0];
             proyecto.presupuesto.egresos = egresos;
-
 
             //para entregables
         }
@@ -139,10 +162,9 @@ async function listarDatosProyectosXGrupo(req, res, next) {
     }
 }
 
-
 module.exports = {
     insertarGrupoProyectos,
     listarGruposProyecto,
     listarProyectosXGrupo,
-    listarDatosProyectosXGrupo
+    listarDatosProyectosXGrupo,
 };
