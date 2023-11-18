31bee30abf56c4a1e2963c14775b3425fd1c0d36
Azsurei <a20203298@pucp.edu.pe>
2023-11-18 10:24:07 -0500

    agregando plan de calidad

--- frontend/src/app/dashboard/[project]/planDeCalidad/page.js
+++ frontend/src/app/dashboard/[project]/planDeCalidad/page.js
@@ -1,14 +1,17 @@
-"use client"
+"use client";
 import axios from "axios";
-import { useState, useEffect,useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { SmallLoadingScreen } from "../layout";
 import { Textarea, Input, Button } from "@nextui-org/react";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { Breadcrumbs, BreadcrumbsItem } from "@/components/Breadcrumb"; 
 axios.defaults.withCredentials = true;
 
 export default function PlanDeCalidad(props) {
+    const keyParamURL = decodeURIComponent(props.params.updateCI);
     const decodedUrl = decodeURIComponent(props.params.project);
+    const projectName = decodedUrl.substring(0, decodedUrl.lastIndexOf("="));
     const projectId = decodedUrl.substring(decodedUrl.lastIndexOf("=") + 1);
     console.log("El id del proyecto es:", projectId);
     const { setIsLoadingSmall } = useContext(SmallLoadingScreen);
@@ -17,6 +20,33 @@ export default function PlanDeCalidad(props) {
         setIsLoadingSmall(false);
     }, []);
     return (
-        <div>hola</div>
+        <div class="flex-1 font-[Montserrat] flex flex-col w-full h-auto pl-8 pr-8">
+            <div class="flex items-center w-full pt-4 pb-3">
+                <Breadcrumbs>
+                    <BreadcrumbsItem
+                        href="/dashboard"
+                        text={"Inicio"}
+                    ></BreadcrumbsItem>
+                    <BreadcrumbsItem
+                        href="/dashboard"
+                        text={"Proyectos"}
+                    ></BreadcrumbsItem>
+                    <BreadcrumbsItem
+                        href={"/dashboard/" + projectName + "=" + projectId}
+                        text={projectName}
+                    ></BreadcrumbsItem>
+                    <BreadcrumbsItem
+                        href={
+                            "/dashboard/" +
+                            projectName +
+                            "=" +
+                            projectId +
+                            "/matrizDeResponsabilidades"
+                        }
+                        text={"Plan de calidad"}
+                    ></BreadcrumbsItem>
+                </Breadcrumbs>
+            </div>
+        </div>
     );
 }
