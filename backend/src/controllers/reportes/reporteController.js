58001fe9986a1a50288a59efd00ccebb1e289730
Sebstret <master-sebas@hotmail.com>
2023-12-01 18:14:11 -0500

    Procedures Grupo de Proyecto y Reportes
    Agregado Modificar Proyecto
    Agregado Eliminar Reporte de Proyecto

--- backend/src/controllers/reportes/reporteController.js
+++ backend/src/controllers/reportes/reporteController.js
@@ -11,6 +11,23 @@ async function listarReportesXIdProyecto(req,res,next){
     }
 }   
 
+async function eliminar(req, res, next) {
+    const { idReporteXProyecto } = req.body;
+    try {
+        const query = `CALL ELIMINAR_REPORTE_PROYECTO(?);`;
+        await connection.query(query, [idReporteXProyecto]);
+        console.log(`Reporte ${idReporteXProyecto} eliminado`);    
+        res.status(200).json({
+            message: "Reporte eliminado exitosamente"
+        });
+    } catch (error) {
+        console.log("Error en la eliminacion ",error);
+        res.status(500).send("Error en la eliminacion: " + error.message);
+        next(error);
+    }
+}
+
 module.exports = {
-    listarReportesXIdProyecto
+    listarReportesXIdProyecto,
+    eliminar
 }
--- backend/src/routes/grupoProyectos.js
+++ backend/src/routes/grupoProyectos.js
@@ -2,11 +2,12 @@ const express = require("express");
 const routerGrupoProyectos = express.Router();
 const { verifyToken } = require("../middleware/middlewares");
 const grupoProyectosController = require("../controllers/grupoProyectos/grupoProyectosController");
+const { verify } = require("jsonwebtoken");
 
-routerGrupoProyectos.post("/insertarGrupoProyectos", grupoProyectosController.insertarGrupoProyectos);
-routerGrupoProyectos.get("/listarGruposProyecto/:idUsuario", grupoProyectosController.listarGruposProyecto);
-routerGrupoProyectos.get("/listarProyectosXGrupo/:idGrupoProyecto", grupoProyectosController.listarProyectosXGrupo);
+routerGrupoProyectos.post("/insertarGrupoProyectos", verifyToken, grupoProyectosController.insertarGrupoProyectos);
+routerGrupoProyectos.get("/listarGruposProyecto/:idUsuario", verifyToken, grupoProyectosController.listarGruposProyecto);
+routerGrupoProyectos.get("/listarProyectosXGrupo/:idGrupoProyecto", verifyToken, grupoProyectosController.listarProyectosXGrupo);
+routerGrupoProyectos.put("/modificarGrupoProyectos",verify, grupoProyectosController.modificar);
 routerGrupoProyectos.delete("/eliminarGrupoProyectos", verifyToken, grupoProyectosController.eliminar);
-
 routerGrupoProyectos.get("/listarDatosProyectosXGrupo/:idGrupoProyecto", grupoProyectosController.listarDatosProyectosXGrupo);
 module.exports.routerGrupoProyectos = routerGrupoProyectos;
