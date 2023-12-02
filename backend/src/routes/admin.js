e2e12a774cc170c468b90553be3545dac370175a
Sebstret <master-sebas@hotmail.com>
2023-12-02 15:23:28 -0500

    Admin Changes

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -1,54 +1,10 @@
 const express = require("express");
 const routerAdmin = express.Router();
-const connection = require('../config/db');
 const { verifyToken } = require("../middleware/middlewares");
+const adminController = require("../controllers/admin/adminController");
 
-routerAdmin.get("/listarUsuariosConPrivilegios",async(req,res)=>{
-
-    console.log("Llegue a recibir solicitud de listar usuarios con privilegios");
-
-    const query = `
-        CALL LISTAR_USUARIOS();
-    `;
-    try {
-        const [results] = await connection.query(query, []);
-        console.log(results[0]);
-
-        res.status(200).json({
-            usuariosPriv: results[0],
-            message: "Usuarios con privilegios listados correctamente",
-        });
-    } catch (error) {
-        console.error("Error al obtener usuarios: ", error);
-        res.status(500).send(
-            "Error al obtener usuarios: " + error.message
-        );
-    }
-});
-
-routerAdmin.put("/cambiarPrivilegioUsuario",async(req,res)=>{
-
-    console.log("Llegue a recibir solicitud de cambiar usuarios con privilegios");
-
-    const { idUsuario, idPrivilegio } = req.body;
-
-    const query = `
-        CALL ACTUALIZAR_PRIVILEGIO_X_IDUSUARIO(?,?);
-    `;
-    try {
-        const [results] = await connection.query(query, [idUsuario,idPrivilegio]);
-        console.log(results[0]);
-
-        res.status(200).json({
-            usuario: results[0],
-            message: "Usuarios modificado correctamente",
-        });
-    } catch (error) {
-        console.error("Error al modificar usuario: ", error);
-        res.status(500).send(
-            "Error al modificar usuario: " + error.message
-        );
-    }
-})
+routerAdmin.get("/listarUsuariosConPrivilegios", adminController.listarUsuariosConPrivilegios);
+routerAdmin.put("/cambiarPrivilegioUsuario", adminController.cambiarPrivilegioUsuario);
+routerAdmin.put("/cambiarEstadoUsuario", adminController.cambiarEstadoUsuario);
 
 module.exports.routerAdmin = routerAdmin;
