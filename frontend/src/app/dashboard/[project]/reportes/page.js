9bf577d3b528687f279a5967a086b0d8b1529b84
Kitaro1 <iwasaki.diego@gmail.com>
2023-12-03 11:29:52 -0500

    mensaje

--- frontend/src/app/dashboard/[project]/reportes/page.js
+++ frontend/src/app/dashboard/[project]/reportes/page.js
@@ -138,20 +138,20 @@ export default function Reportes(props) {
                 projectId +
                 "/reportes/reporteRiesgos/nuevoReporte",
         },
-        {
-            id: "rep5",
-            title: "Reporte de Grupo de Proyectos",
-            description:
-                "Un reporte de grupo de proyectos es un resumen estructurado que detalla información relevante sobre varios proyectos agrupados en función de ciertos criterios comunes" +
-                "Proporciona una visión global de múltiples iniciativas en un solo documento, permitiendo una evaluación eficiente de la situación y el progreso colectivo de proyectos relacionados.",
-            imgLink: "/images/report_4.webp",
-            hrefGoTo:
-                "/dashboard/" +
-                projectName +
-                "=" +
-                projectId +
-                "/reportes/reporteGrupoProyectos",
-        },
+        // {
+        //     id: "rep5",
+        //     title: "Reporte de Grupo de Proyectos",
+        //     description:
+        //         "Un reporte de grupo de proyectos es un resumen estructurado que detalla información relevante sobre varios proyectos agrupados en función de ciertos criterios comunes" +
+        //         "Proporciona una visión global de múltiples iniciativas en un solo documento, permitiendo una evaluación eficiente de la situación y el progreso colectivo de proyectos relacionados.",
+        //     imgLink: "/images/report_4.webp",
+        //     hrefGoTo:
+        //         "/dashboard/" +
+        //         projectName +
+        //         "=" +
+        //         projectId +
+        //         "/reportes/reporteGrupoProyectos",
+        // },
     ];
 
     const [listReportes, setListReportes] = useState([]);
